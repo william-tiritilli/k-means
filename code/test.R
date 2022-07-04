42518a50523cf74ebd8217acdce0a025cfd50e47
William Tiritilli <William.Tiritilli@iaawg.com>
2022-07-03 21:36:46 -0500

    back to first step

--- code/test.R
+++ code/test.R
@@ -1,11 +1,4 @@
-get_yesterday <-function(){
-  
-<<<<<<< HEAD
-  return(Sys.Date()-1)
-}
+# initial commit
 
-=======
-  x<-(as.character(Sys.Date()-1))
-  return(x)
-}
->>>>>>> 4416a62659a938e398be718d4a7a71ff998584c5
+
+1+1
